5930040eb6a19b16bba13b794974004603da2d68
ygvbhy <ygvbhy@naver.com>
2024-11-03 18:15:19 +0900

    nuxt components

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div class="q-my-xl tex-center">
+      <div class="text-h4">All Courese</div>
+      <p class="q-mt-sm text-grey-8">웹 개발 입문부터 실전까지 학습해보세요</p>
+    </div>
+    <div class="row q-col-gutter-lg">
+      <div v-for="n in 3" :key="n" class="col-12 col-md-4 col-sm-6">
+        <CourseCard />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
