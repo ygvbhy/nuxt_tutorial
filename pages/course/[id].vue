5a31ec5afdc94ffc28c27d7e3a4a28e2b54d23a6
ygvbhy <ygvbhy@naver.com>
2024-11-03 18:46:25 +0900

    파일 기반 라우팅

---
+++ pages/course/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <p-page padding>
+    <h2>course</h2>
+    <div class="text-subtitle1">
+      {{ $route.params }}
+    </div>
+  </p-page>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<style scoped></style>
