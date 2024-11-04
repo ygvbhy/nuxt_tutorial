fa919960d6696580ebaaa68ca18de0b44dd15260
ygvbhy <ygvbhy@naver.com>
2024-11-04 21:25:34 +0900

    NuxtPage 내장 컴포넌트

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -2,11 +2,19 @@ import type { CourseWithPath } from '~/types/course';
 
 interface CourseReturn {
   course: Maybe<CourseWithPath>;
+  prevCourse: Maybe<CourseWithPath>;
+  nextCourse: Maybe<CourseWithPath>;
 }
 export const useCourse = (courseSlug: string): CourseReturn => {
   const { courses } = useCourses();
-  const course = courses.find((course) => course.courseSlug === courseSlug);
+  // const course = courses.find((course) => course.courseSlug === courseSlug);
+  const index = courses.findIndex((course) => course.courseSlug === courseSlug);
+  const course = courses[index];
+  const prevCourse = index <= 0 ? null : courses[index - 1];
+  const nextCourse = index >= courses.length - 1 ? null : courses[index + 1];
   return {
     course,
+    prevCourse,
+    nextCourse,
   };
 };
--- pages/course/[courseSlug].vue
+++ pages/course/[courseSlug].vue
@@ -45,6 +45,29 @@
       <p class="q-mt-lg text-grey-8">
         {{ course?.content }}
       </p>
+      <template #footer>
+        <q-btn
+          v-if="prevCourse"
+          label="이전 강의"
+          color="primary"
+          unelevated
+          :to="prevCourse"
+        />
+        <q-btn
+          label="쿼리 추가"
+          color="dark"
+          unelevated
+          :to="{ path: $route.path, query: { timestamp: Date.now() } }"
+        />
+        <q-space />
+        <q-btn
+          v-if="nextCourse"
+          label="다음 강의"
+          color="primary"
+          unelevated
+          :to="nextCourse"
+        />
+      </template>
     </AppCard>
   </div>
 </template>
@@ -52,7 +75,11 @@
 <script setup lang="ts">
 const route = useRoute();
 const courseSlug = route.params.courseSlug as string;
-const { course } = useCourse(courseSlug);
+const { course, prevCourse, nextCourse } = useCourse(courseSlug);
+console.log('[courseSlug].vue 컴포넌트 setup hooks');
+definePageMeta({
+  key: (route) => route.fullPath,
+});
 </script>
 
 <style scoped></style>
