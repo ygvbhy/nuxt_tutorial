7434c237de6e0213b2eb93cbbbbe8317d78b66f0
ygvbhy <ygvbhy@naver.com>
2024-11-05 21:27:07 +0900

    NuxtLink 내장 컴포넌트

--- pages/about.vue
+++ pages/about.vue
@@ -5,6 +5,17 @@
       <p class="q-mt-sm text-grey-8">
         Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.
       </p>
+      <div class="column">
+        <router-link to="/">RouterLink Home</router-link>
+        <router-link to="https://youtube.com/@gymcoding">
+          RouterLink Youtube
+        </router-link>
+        <br />
+        <NuxtLink to="/">RouterLink Home</NuxtLink>
+        <NuxtLink to="https://youtube.com/@gymcoding">
+          RouterLink Youtube
+        </NuxtLink>
+      </div>
     </div>
   </q-page>
 </template>
