2fee76d5b35d9044cd708cc49a44c708035ac887
sypark <sangyune.park@k-futuretech.com>
2024-11-08 13:23:10 +0900

    Nuxt 상태관관리 - useState()

--- pages/about.vue
+++ pages/about.vue
@@ -15,9 +15,39 @@
         <NuxtLink to="https://youtube.com/@gymcoding">
           RouterLink Youtube
         </NuxtLink>
+
+        <div class="q-gutter-y-sm q-mt-md">
+          <div class="text-subtitle1 text-weight-bold">useState('counter')</div>
+          <div>
+            counter: {{ counter }}
+            <q-btn
+              label="counter"
+              color="primary"
+              dense
+              no-caps
+              @click="counter++"
+            />
+          </div>
+          <div>
+            sameCounter: {{ sameCounter }}
+            <q-btn
+              label="sameCounter"
+              color="primary"
+              dense
+              no-caps
+              @click="sameCounter++"
+            />
+          </div>
+          <div>
+            <q-btn label="clear" @click="clearNuxtState()" />
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const counter = useState<number>('counter', () => 1);
+const sameCounter = useState<number>('counter');
+</script>
