fca01e6d0a0a02b536d2c8d2f3756641e32ac87e
ygvbhy <ygvbhy@naver.com>
2024-11-09 22:42:36 +0900

    Server Routes - Auth API

--- composables/useCourses.ts
+++ composables/useCourses.ts
@@ -2,10 +2,15 @@ import type { CourseWithPath } from '~/types/course';
 
 interface CoursesReturn {
   courses: CourseWithPath[];
+  // 제네릭 타입으로 추가 한다면 이렇게 추가 하면 됨
+  // 1
+  // courses: Maybe<CourseWithPath[]>;
 }
 
 export const useCourses = async (): Promise<CoursesReturn> => {
   const { data, error } = await useFetch('/api/courses');
+  // 2
+  // const { data, error } = await useFetch<CourseWithPath[]>('/api/courses');
 
   if (error.value) {
     throw createError({
@@ -13,6 +18,8 @@ export const useCourses = async (): Promise<CoursesReturn> => {
     });
   }
   return {
+    // 3
+    // courses: data.value
     courses: data.value as CourseWithPath[],
   };
 };
---
+++ server/routes/auth/login.post.ts
@@ -0,0 +1,42 @@
+import { getUserByEmail } from '~/server/models/user';
+import { verifyPassword } from '~/server/utils/password';
+
+export default defineEventHandler(async (event) => {
+  const body = readBody<{ email: string; password: string }>(event);
+
+  const { email, password } = await body;
+
+  if (!email || !password) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Email address and Password are required',
+    });
+  }
+
+  const userWithPassword = getUserByEmail(email);
+
+  if (!userWithPassword) {
+    throw createError({
+      statusMessage: 'Bad credentials',
+      statusCode: 401,
+    });
+  }
+
+  const verified = verifyPassword(password, userWithPassword.password);
+
+  if (!verified) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Bad credentials',
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: _password, ...userWithoutPassword } = userWithPassword;
+
+  setCookie(event, '__user', JSON.stringify(userWithPassword));
+
+  return {
+    user: userWithPassword,
+  };
+});
